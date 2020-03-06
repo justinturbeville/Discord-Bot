79b75ba7ad86cc551b9f03995295303cc7c6a42e
justinturbeville <59580400+justinturbeville@users.noreply.github.com>
2020-03-06 00:20:18 -0500

    Add files via upload

---
+++ Discord  Bot.py
@@ -0,0 +1,10 @@
+from discord_webhook import DiscordWebhook
+
+message = raw_input("Enter a message:")
+#This take the users input for a message to be sent
+
+webhook = DiscordWebhook(url='https://discordapp.com/api/webhooks/685347391582240850/OrpXYxhJJxvEtXJDEWDUWwNoEtxT2btUBl7-lG9lbv3Zqpg1G4njofypigdcVAc2w-Px', $#uses the webhook to send the message
+#the webhook contains the server and channel information
+
+response = webhook.execute()
+#sends the message
